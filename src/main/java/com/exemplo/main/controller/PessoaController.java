fef020dd980897ec6d60d34c01655c14f55fbf6e
unknown <marcelocrisknauer@gmail.com>
2022-03-29 01:23:20 -0300

    Incluir pessoa

--- src/main/java/com/exemplo/main/controller/PessoaController.java
+++ src/main/java/com/exemplo/main/controller/PessoaController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.exemplo.main.models.Pessoa;
 import com.exemplo.main.repositories.PessoaRepository;
 
 @Controller
@@ -17,5 +20,16 @@ public class PessoaController {
 	public String listAll(Model model) {
 		model.addAttribute("listaPessoas", ps.findAll());
 		return"rh/pessoas/index";
+	}
+	
+	@GetMapping("rh/pessoas/nova")
+	public String novaPessoa(@ModelAttribute("pessoa") Pessoa pessoa) {
+		return "rh/pessoas/form";
+	}
+	
+	@PostMapping("rh/pessoas/salvar")
+	public String salvar(@ModelAttribute("pessoa") Pessoa pessoa) {
+		ps.save(pessoa);
+		return "redirect:/rh/pessoas";
 	}
 }
--- src/main/java/com/exemplo/main/models/Pessoa.java
+++ src/main/java/com/exemplo/main/models/Pessoa.java
@@ -19,7 +19,7 @@ public class Pessoa implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
-	@DateTimeFormat(style =  "yyyy-MM-dd")
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private LocalDate dtNascimento;
 	private String cpf;
 	private String email;
